c17e3d7d6a71975a2ce0b7a1903674a006ad7a65
Carolina de Freitas Manso <carolcarolmanso@gmail.com>
2018-10-29 20:04:50 -0200

    mexi na triangularização da matriz, aparentemente tá fazendo certo porémhá erro na conferencia dos limites inferiores da matriz, por isso ta dando segmentation fault

--- pentaDiagonal .c
+++ pentaDiagonal .c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <math.h>
 void imprimeM(int n, float** m);
 void leMatriz(float**m,int n,float dA,float dB,float dP,float dC,float dD);
 float** criaMatriz(int n);
 void liberaMatriz(int n, float** m);
 float* criaB(int n, float**m);
+void triangulariza(int n, float** m, float* b);
 
 int main(){
 	int n;
 	float** m;
 	float* b;
 	float dA,dB, dP, dC, dD;
-	printf("insira um m e os bagulhos...");
+
+	printf("Insira o valor de N: ");
 	scanf("%d",&n);
+	printf("Digite o valor de dA: ");
+	scanf("%f", &dA);
+	printf("Digite o valor de dB: ");
+	scanf("%f", &dB);
+	printf("Digite o valor de dP: ");
+	scanf("%f", &dP);
+	printf("Digite o valor de dC: ");
+	scanf("%f", &dC);
+	printf("Digite o valor de dD: ");
+	scanf("%f", &dD);
 	m = criaMatriz(n);
 	b = criaB(n,m);
-	scanf(" %f %f %f %f %f", &dA, &dB, &dP,&dC, &dD);
 	leMatriz(m,n,dA,dB,dP,dC,dD);
 	imprimeM(n,m);
+	triangulariza(n, m, b);
+	imprimeM(n, m);
 	liberaMatriz(n,m);
 	return 0; 
 }
@@ -50,10 +63,10 @@ float** criaMatriz(int n){
 float* criaB(int n, float**m){
 	float* b = (float*) malloc (n*sizeof(float));
 	
-	for(i=0;i<n;i++)
+	for(int i=0;i<n;i++)
     {
       b[i]=0;
-      for(j=0;j< n;j++)
+      for(int j=0;j< n;j++)
       {
            b[i]= b[i] + m[i][j];
       }
@@ -79,70 +92,86 @@ void liberaMatriz(int n, float** m){
 	free(m);
 }
 
-void jacobi(int n, float** m){
+/*void jacobi(int n, float** m){
 
 }
 
 void seidel(int n, float** m){
 
-}
+}*/
 
-void triangulariza(int n, float** m){
+void triangulariza(int n, float** m, float* b){
 	
 	int indicePivo;
-	float aux;
+	float aux, maior, mult;
 	
 	for(int k=0;k<(n-1);k++)
     {
-       printf("\n--------- Etapa %d -------------\n", k);
-       
-       //acha maior
        indicePivo = k;
-       maior = fabs(A[k][k]);
-       for(i=k+1; i<=(n-1);i++){
-       		if(fabs(A[i][k]) > maior){
-       			maior = fabs(A[i][k]);
-       			indicePivo = i;
-       		}
+       maior = fabs(m[k][k]);
+       for(int i=k+1; i<= k+2;i++){
+			if(i >= n-2){
+				if(i == n-2){
+					if(fabs(m[i][k]) > maior){
+						maior = fabs(m[i][k]);
+						indicePivo = i;
+					}
+				}
+					
+			}
+				
+			else{
+		   		if(fabs(m[i][k]) > maior){
+		   			maior = fabs(m[i][k]);
+		   			indicePivo = i;
+		   		}
+			}
        }
        
        //troca linha pela maior
        if(indicePivo != k){
-       		for(j=k; j<=n-1;j++){
-       			aux = A[k][j];
-       			A[k][j] = A[indicePivo][j];
-       			A[indicePivo][j] = aux;
+       		for(int j = k; j <= n-1; j++){
+       			aux = m[k][j];
+       			m[k][j] = m[indicePivo][j];
+       			m[indicePivo][j] = aux;
        		}
        		aux = b[k];
        		b[k] = b[indicePivo];
        		b[indicePivo] = aux;
        }
-        
-       for(i=(k+1);i<n;i++)
-       {
-           m = A[i][k]/A[k][k];
-           //A[i][k]=0; // para visualização da matriz triangularizada
-           printf("   m = %f;", m);
+/*	 Impressão da matriz intermediaria
+		imprimeM(n, m);
+		printf("\n");*/
+       
+
+	// NÃO TERMINADO !!! : acho q ta fazendo certo porém ta dando segmentation fault por causa dos limites da matriz
+       for(int i=(k+1);i<= k+2;i++){
+
+			/*alguma coisa errada nesses ifs que é pra olhar as duas ultimas posições da diagonal principal que se comportam diferente das outras linhas*/
+			if(i >= n-2){
+				if(k == n-2){
+					 mult = m[i][k]/m[k][k];
+					i += 2;
+				}
+			}
+			else{
+           mult = m[i][k]/m[k][k];	
+			}
+           m[i][k]=0; // para visualização da matriz triangularizada
+           printf("   m = %f;", mult);
          
            //for(j=(k+1);j<n;j++)
-           for(j=(k);j<n;j++)
+           for(int j=(k);j<n;j++)
            {
-              A[i][j]= A[i][j]- m*A[k][j];
+              m[i][j]= m[i][j]- mult*m[k][j];
            } // fim  j
-           b[i]= b[i]- m*b[k];
+           b[i]= b[i]- mult*b[k];
 
         } // fim linha i
 
       // Mostrando a matriz intermediaria
       printf("\n - Matriz apos a etapa %d ------\n", k);
-      for(i=0;i< n;i++)
-     {
-       for(j=0;j< n;j++)
-       {
-        printf("  %15.6f ", A[i][j]);
-        }
-        printf(" | b[%d]: %10.3f\n", i, b[i]);
-      }
+    	imprimeM(n,m);
 
 
     }
