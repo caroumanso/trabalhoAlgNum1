32e3048b8c15e2e8a504b473817ce1148f3de583
murilo <muriloborghardt7@gmail.com>
2018-10-30 14:31:54 -0300

    Começando seidel

--- pentaDiagonal .c
+++ pentaDiagonal .c
@@ -12,6 +12,8 @@ float* criaB(int n, float**m);
 void triangulariza(int n, float** m, float* b);
 float* substituicaoRegressiva(int n, float** m , float* b);
 void imprimeVetor(int n, float* a);
+void gauss(int n, float** m);
+
 
 int main(){
 	int n;
@@ -34,8 +36,23 @@ int main(){
 	scanf("%f", &dD);
 	
 	m = criaMatriz(n);
-	
 	leMatriz(m,n,dA,dB,dP,dC,dD);
+	
+	//
+	gauss(n,m);
+	
+	//
+	seidel(n,m);
+	
+	liberaMatriz(n,m);
+	return 0; 
+}
+
+
+
+//
+void gauss(int n, float** m){
+	
 	b = criaB(n, m);
 	
 	imprimeM(n, m, b);
@@ -46,11 +63,145 @@ int main(){
 	x = substituicaoRegressiva(n, m, b);
 	imprimeVetor(n,x);
 	
-	liberaMatriz(n,m);
+	free(b);
+	free(x);
 	printf("\nNumero de operações: %d\n",op);
-	
-	return 0; 
 }
+
+
+
+/*
+void seidel(int n, float** m){
+	float tol = 0.000001;
+    float distRel = 1.0;
+    float d[N];
+    int dmax,xmax;
+    
+    while(distRel > tol){
+      iter = 0;
+      iter++;
+      dmax = 0;
+      xmax = 0;
+      for(i=0;i< n;i++)
+      {
+        soma=0;
+        for(j=0;j<i ;j++)
+        {
+          soma = soma + A[i][j]*X[j];
+        }
+      printf("\n Vetor1");
+        for(j=(i+1);j<n;j++)
+        {
+          soma = soma + A[i][j]*XA[j];
+        }
+      X[i]= (b[i]-soma )/A[i][i];
+      if(X[i] > xmax){
+        xmax = X[i];
+      }
+    printf("\n Vetor2");
+      d[i]=fabs(XA[i]-X[i]);        
+      if(d[i]>dmax){
+        dmax = d[i];
+      }
+      } // fim for i
+  distRel = dmax/xmax;
+}*/
+
+//
+void triangulariza(int n, float** m, float* b){
+	op = 0;	
+	int indicePivo, auxInt;
+	float aux, maior, mult;
+
+	for(int k=0;k<(n-1);k++)
+    {
+       indicePivo = k;
+       maior = fabs(m[k][k]);
+       for(int i=k+1; i<= k+2;i++){
+			if(k >= n-2){
+				if(k == n-2){
+					if(fabs(m[i][k]) > maior){
+						maior = fabs(m[i][k]);
+						indicePivo = i;
+					}
+					i++;
+				}
+					
+			}
+				
+			else{
+		   		if(fabs(m[i][k]) > maior){
+		   			maior = fabs(m[i][k]);
+		   			indicePivo = i;
+		   		}
+			}
+       } 
+       //troca linha pela maior
+       if(indicePivo != k){
+       		for(int j = k; j <= n-1; j++){
+       			aux = m[k][j];
+       			m[k][j] = m[indicePivo][j];
+       			m[indicePivo][j] = aux;
+       		}
+       		aux = b[k];
+       		b[k] = b[indicePivo];
+       		b[indicePivo] = aux;
+       }
+	
+   	 	for(int i=(k+1);i<= k+2;i++){
+   	 		auxInt = i;
+			if(k >= n-2){
+				if(k == n-2){
+					 mult = m[i][k]/m[k][k];
+					i += 2;
+				}
+			}
+			else{
+           		mult = m[i][k]/m[k][k];
+			}
+			op++;
+        	m[auxInt][k]=0; // para visualização da matriz triangularizada
+        	printf("   m = %f;", mult);
+         
+           //for(j=(k+1);j<n;j++)
+        	for(int j=k+1;j<n;j++)
+        	{
+        		m[auxInt][j]= m[auxInt][j] - mult*m[k][j];
+        		op = op +2;
+        	} // fim  j
+           b[auxInt]= b[auxInt]- mult*b[k];
+				op = op + 2;
+        } // fim linha i
+	
+      // Mostrando a matriz intermediaria
+      printf("\n - Matriz apos a etapa %d ------\n", k);
+    	imprimeM(n, m, b);
+		printf("\n");
+	
+
+    }
+}
+
+float* substituicaoRegressiva(int n, float** m , float* b){
+	float* x = criaX(n);
+	float soma;
+	
+	x[n-1]= b[n-1]/m[n-1][n-1]; 
+    for(int i=(n-2);i>=0;i--)  
+    {
+      soma=b[i];
+      for(int j=i+1;(j<n)&&(j<(i+5));j++)
+      {
+        soma = soma - m[i][j]*x[j];
+      }
+      x[i]= soma/m[i][i];
+      printf(" %f\n",soma);
+    }
+    
+    return x;
+}
+
+// ----------------------------------------   Auxiliares  ---------------------------------------------
 
 void leMatriz(float**m,int n,float dA,float dB,float dP,float dC,float dD){
 	int i,j, sub;
@@ -113,11 +264,9 @@ void imprimeM(int n, float** m, float* b){
 		printf("\n");
 		 
 	}
-	//printf("\n--------------- Fim  ---------------\n"); 
 }
 
 void imprimeVetor(int n, float* a){
-	printf("");
 	for(int i=0;i<n;i++){
 		printf("| %f |",a[i]);
 		printf("\n");
@@ -132,119 +281,4 @@ void liberaMatriz(int n, float** m){
 	free(m);
 }
 
-/*void jacobi(int n, float** m){
 
-}
-
-void seidel(int n, float** m){
-
-}*/
-
-void triangulariza(int n, float** m, float* b){
-	op = 0;	
-	int indicePivo, auxInt;
-	float aux, maior, mult;
-
-	for(int k=0;k<(n-1);k++)
-    {
-       indicePivo = k;
-       maior = fabs(m[k][k]);
-       for(int i=k+1; i<= k+2;i++){
-			if(k >= n-2){
-				if(k == n-2){
-					if(fabs(m[i][k]) > maior){
-						maior = fabs(m[i][k]);
-						indicePivo = i;
-					}
-					i++;
-				}
-					
-			}
-				
-			else{
-		   		if(fabs(m[i][k]) > maior){
-		   			maior = fabs(m[i][k]);
-		   			indicePivo = i;
-		   		}
-			}
-       }
-       
-       //troca linha pela maior
-       if(indicePivo != k){
-       		for(int j = k; j <= n-1; j++){
-       			aux = m[k][j];
-       			m[k][j] = m[indicePivo][j];
-       			m[indicePivo][j] = aux;
-       		}
-       		aux = b[k];
-       		b[k] = b[indicePivo];
-       		b[indicePivo] = aux;
-       }
-/*	 Impressão da matriz intermediaria
-		imprimeM(n, m);
-		printf("\n");*/
-       
-
-	
-   	 	for(int i=(k+1);i<= k+2;i++){
-   	 		auxInt = i;
-			if(k >= n-2){
-				if(k == n-2){
-					 mult = m[i][k]/m[k][k];
-					i += 2;
-				}
-			}
-			else{
-           		mult = m[i][k]/m[k][k];
-			}
-			op++;
-        	m[auxInt][k]=0; // para visualização da matriz triangularizada
-        	printf("   m = %f;", mult);
-         
-           //for(j=(k+1);j<n;j++)
-        	for(int j=k+1;j<n;j++)
-        	{
-        		m[auxInt][j]= m[auxInt][j] - mult*m[k][j];
-        		op = op +2;
-        	} // fim  j
-           b[auxInt]= b[auxInt]- mult*b[k];
-				op = op + 2;
-        } // fim linha i
-	
-      // Mostrando a matriz intermediaria
-      printf("\n - Matriz apos a etapa %d ------\n", k);
-    	imprimeM(n, m, b);
-		printf("\n");
-	
-
-    }
-}
-/*
-  3.000  2.000  1.000  1.000  2.000  0.000  0.000  0.000 | 9.000 
-  0.000  3.000  2.000  1.000  1.000  2.000  0.000  0.000 | 9.000 
-  0.000  0.000  3.000  2.000  1.000  1.000  2.000  0.000 | 9.000 
-  0.000  0.000  0.000  3.000  2.000  1.000  1.000  2.000 | 9.000 
-  0.000  0.000  0.000  0.000  3.000  2.000  1.000  1.000 | 7.000 
-  0.000  0.000  0.000  0.000  0.000  3.000  2.000  1.000 | 6.000 
-  0.000  0.000  0.000  0.000  0.000  0.000  -0.750  -0.373 | -1.123 
-  0.000  0.000  0.000  0.000  0.000  0.000  0.000  1.238 | 1.238 
-
-*/
-float* substituicaoRegressiva(int n, float** m , float* b){
-	float* x = criaX(n);
-	float soma;
-	
-	x[n-1]= b[n-1]/m[n-1][n-1]; 
-    for(int i=(n-2);i>=0;i--)  
-    {
-      soma=b[i];
-      for(int j=i+1;(j<n)&&(j<(i+5));j++)
-      {
-        soma = soma - m[i][j]*x[j];
-      }
-      x[i]= soma/m[i][i];
-      printf(" %f\n",soma);
-    }
-    
-    return x;
-}
